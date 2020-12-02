a81f0ee6839ee1d0cacf45cdc2b28daeba753882
Kalu Chinonso <tripletens.kc@gmail.com>
2020-12-02 09:11:08 +0100

    new changes

---
+++ build/precache-manifest.e10771879fce778b084e467c379ef849.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cf2d40ea4f084efe2a259430e0838d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a48c70d6637ec8640a",
+    "url": "/static/js/2.552dcd59.chunk.js"
+  },
+  {
+    "revision": "f337e7a15107d5185e7c",
+    "url": "/static/js/main.67d14b22.chunk.js"
+  },
+  {
+    "revision": "3a3f636253ba2da2c078",
+    "url": "/static/js/runtime-main.7bc58992.js"
+  }
+]);
